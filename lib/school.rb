7717d9387218ee96da5a23a5cbef7b0eac52867e
amy-chinlai <amy@actionnetwork.org>
2021-07-06 15:25:41 -0400

    commit tree 54d0735b638f227180662e30a7b5832c29b9b981

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+
+attr_accessor :roster, :name, :grade
+
+    def initialize(roster)
+    roster = {}
+    @roster = roster
+end
+
+def add_student(name, grade)
+    if roster.key?(grade) == false
+        roster[grade] = []
+    end
+    roster[grade] << "#{name}"
+end
+
+def grade(grade)
+    roster.fetch(grade)
+end
+
+def sort
+    # roster.sort.to_h
+    # roster.sort_by {|grade, name| [-name, grade]}.to_h
+    sorted_roster = {}
+    roster.keys.each do |grade|
+        sorted_roster[grade] = roster[grade].sort
+    end
+    sorted_roster
+end
+
+end
