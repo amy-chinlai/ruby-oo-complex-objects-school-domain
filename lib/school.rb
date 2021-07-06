6999abc925267c75ed036ddb5ea3e9fd8c008e97
amy-chinlai <amy@actionnetwork.org>
2021-07-06 15:25:50 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+
+attr_accessor :roster, :name, :grade
+
+    def initialize(roster)
+    roster = {}
+    @roster = roster
+end
+
+def add_student(name, grade)
+    if roster.key?(grade) == false
+        roster[grade] = []
+    end
+    roster[grade] << "#{name}"
+end
+
+def grade(grade)
+    roster.fetch(grade)
+end
+
+def sort
+    sorted_roster = {}
+    roster.keys.each do |grade|
+        sorted_roster[grade] = roster[grade].sort
+    end
+    sorted_roster
+end
+
+end
